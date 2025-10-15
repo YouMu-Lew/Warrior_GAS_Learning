f963b382e631f5a7baf52ca1058e2de139f85dd3
YouMu <lewyoumu@gmail.com>
2025-10-15 21:21:00 +0800

    Section_9 : Add Native Gameplay Tags

---
+++ Source/Warrior_GAS_Learning/Public/WarriorGameplayTags.h
@@ -0,0 +1,13 @@
+// YouMu All Rights Reserved.
+
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace WarriorGameplayTags {
+
+    /** Input Tags */
+    WARRIOR_GAS_LEARNING_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Move);
+    WARRIOR_GAS_LEARNING_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Look);
+
+} // namespace WarriorGameplayTags
