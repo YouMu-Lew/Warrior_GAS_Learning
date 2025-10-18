4f3fca1fbadf607c8f3b18a8f5415fd4b7877646
YouMu <lewyoumu@gmail.com>
2025-10-18 14:28:17 +0800

    Section_14: Add new state `Relax` in HeroCharacter AnimBlueprint

--- Source/Warrior_GAS_Learning/Public/AnimInstances/Hero/WarriorHeroAnimInstance.h
+++ Source/Warrior_GAS_Learning/Public/AnimInstances/Hero/WarriorHeroAnimInstance.h
@@ -6,12 +6,30 @@
 #include "AnimInstances/WarriorCharacterAnimInstance.h"
 #include "WarriorHeroAnimInstance.generated.h"
 
+class AWarriorHeroCharacter;
+
 /**
- * 
+ *
  */
 UCLASS()
 class WARRIOR_GAS_LEARNING_API UWarriorHeroAnimInstance : public UWarriorCharacterAnimInstance
 {
-	GENERATED_BODY()
-	
+    GENERATED_BODY()
+
+public:
+    virtual void NativeInitializeAnimation() override;
+
+    virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds) override;
+
+protected:
+    UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "AnimData|References")
+    AWarriorHeroCharacter* OwningHeroCharacter;
+
+    UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "AnimData|LocomotionData")
+    bool bShouldEnterRelaxState;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "AnimData|LocomotionData")
+    float EnterRelaxStateThreshold = 5.f;
+
+    float IdleElapsedTime;
 };
