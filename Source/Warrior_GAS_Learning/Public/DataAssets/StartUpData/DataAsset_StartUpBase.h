e197e8c85eb28432ed39d9439b00a3817b0610d8
YouMu <lewyoumu@gmail.com>
2025-11-04 16:09:47 +0800

    Section_20: Add DataAsset files

---
+++ Source/Warrior_GAS_Learning/Public/DataAssets/StartUpData/DataAsset_StartUpBase.h
@@ -0,0 +1,34 @@
+// YouMu All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "DataAsset_StartUpBase.generated.h"
+
+class UWarriorGameplayAbility;
+class UWarriorAbilitySystemComponent;
+
+/**
+ *
+ */
+UCLASS()
+class WARRIOR_GAS_LEARNING_API UDataAsset_StartUpBase : public UDataAsset
+{
+    GENERATED_BODY()
+
+public:
+    virtual void GiveToAbilitySystemComponent(UWarriorAbilitySystemComponent* InASCToGive,
+                                              int32 ApplyLevel = 1);
+
+protected:
+    UPROPERTY(EditDefaultsOnly, Category = "StartUpData")
+    TArray<TSubclassOf<UWarriorGameplayAbility>> ActivateOnGivenAbilities;
+
+    UPROPERTY(EditDefaultsOnly, Category = "StartUpData")
+    TArray<TSubclassOf<UWarriorGameplayAbility>> ReactiveAbilities;
+
+protected:
+    void GrantAbilities(const TArray<TSubclassOf<UWarriorGameplayAbility>>& InAbilitiesToGive,
+                        UWarriorAbilitySystemComponent* InASCToGive, int32 ApplyLevel);
+};
