b52d22aa2fb9283d3c6202e69f14cbbb48bd557f
YouMu <lewyoumu@gmail.com>
2026-01-05 22:23:19 +0800

    feat: 新增 LightAttack_Axe Tag 及 Ability

--- Source/Warrior_GAS_Learning/Public/AbilitySystem/Abilities/WarriorGameplayAbility.h
+++ Source/Warrior_GAS_Learning/Public/AbilitySystem/Abilities/WarriorGameplayAbility.h
@@ -26,20 +26,16 @@ class WARRIOR_GAS_LEARNING_API UWarriorGameplayAbility : public UGameplayAbility
 protected:
     //~ Begin UGameplayAbility Interface.
     /** Called when the ability is given to an AbilitySystemComponent */
-    virtual void OnGiveAbility(const FGameplayAbilityActorInfo* ActorInfo,
-                               const FGameplayAbilitySpec& Spec) override;
+    virtual void OnGiveAbility(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec) override;
 
     /** Native function, called if an ability ends normally or abnormally. If bReplicate is set to
      * true, try to replicate the ending to the client/server */
-    virtual void EndAbility(const FGameplayAbilitySpecHandle Handle,
-                            const FGameplayAbilityActorInfo* ActorInfo,
-                            const FGameplayAbilityActivationInfo ActivationInfo,
-                            bool bReplicateEndAbility, bool bWasCancelled) override;
+    virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+                            const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
     //~ End UGameplayAbility Interface.
 
     UPROPERTY(EditDefaultsOnly, Category = "WarriorAbility")
-    EWarriorAbilityActivationPolicy AbilityActivationPolicy =
-        EWarriorAbilityActivationPolicy::OnTriggered;
+    EWarriorAbilityActivationPolicy AbilityActivationPolicy = EWarriorAbilityActivationPolicy::OnTriggered;
 
     UFUNCTION(BlueprintPure, Category = "Warrior|Ability")
     UPawnCombatComponent* GetPawnCombatComponentFromActorInfo() const;
